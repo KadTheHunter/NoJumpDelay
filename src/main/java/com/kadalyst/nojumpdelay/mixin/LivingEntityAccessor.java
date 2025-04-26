e0551c0d3e69522528c48037d7d78ef023199db9
xlzv <96470093+xlzv@users.noreply.github.com>
2025-04-26 23:02:08 +0900

    Rewrite

---
+++ src/main/java/com/kadalyst/nojumpdelay/mixin/LivingEntityAccessor.java
@@ -0,0 +1,11 @@
+package com.kadalyst.nojumpdelay.mixin;
+
+import net.minecraft.entity.LivingEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(LivingEntity.class)
+public interface LivingEntityAccessor {
+    @Accessor("jumpingCooldown")
+    void setJumpingCooldown(int value);
+}
